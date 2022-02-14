02ff16f0833c18f9cdef32969ba99d59b7ffba29
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2022-02-14 20:23:51 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,16 @@
+//let day= +prompt("Enter a number"); //takin input from user
+let day = 2;
+switch(day)
+{
+    case 0:
+        console.log("Sunday");
+        break;
+        
+    case 1:
+        console.log("Monday");
+        break;
+
+    case 2:
+        console.log("Monday");
+        break;
+}
